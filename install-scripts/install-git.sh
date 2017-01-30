f17b5dcb138e56598061e47c83eb291e487aacfa
Magnus Boye <magnusboye@gmail.com>
2017-01-30 12:25:54 +0200

    install: Remove timestamping argument from wget

--- install-scripts/install-git.sh
+++ install-scripts/install-git.sh
@@ -6,10 +6,10 @@ apt-get update
 apt-get install -y git
 
 mkdir -p ~/.scripts
-wget -N -O ~/.scripts/git-prompt.sh \
+wget -O ~/.scripts/git-prompt.sh \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
-wget -N -O ~/.scripts/git-completion.bash \
+wget -O ~/.scripts/git-completion.bash \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 git --version
