e92dc1b19f01fce29433bdfed8f24b86ff1746e4
Magnus Boye <magnusboye@gmail.com>
2017-01-31 11:36:26 +0200

    install-scripts: Install YCM for vim

--- install-scripts/install-vim-plugins.sh
+++ install-scripts/install-vim-plugins.sh
@@ -5,4 +5,14 @@ git clone https://github.com/tpope/vim-pathogen.git ~/.vim
 
 mkdir ~/.vim/bundle
 cd ~/.vim/bundle
+
 git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/airblade/vim-gitgutter.git
+
+apt-get install -y cmake \
+                   python-dev \
+                   python3-dev
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer
